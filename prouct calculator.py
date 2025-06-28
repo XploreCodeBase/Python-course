ef49af0eb22b687ac4d65e4361aec64e8384f12b
XploreCodeBase <support@xplorehub.pl>
2025-06-28 17:59:46 +0200

    Kalkulator parametrów produktu

---
+++ prouct calculator.py
@@ -0,0 +1,24 @@
+# Zmienna typu float
+base_price = 1299.99
+
+# Obliczenie ceny z podatkiem VAT
+price_with_tax = base_price * 1.23
+
+# Zmienna typu boolean
+discount_available = True
+
+# Zmienna typu complex
+wave_component = 2 + 3j
+
+# Wyświetlanie wartości i ich typów
+print(base_price)
+print(type(base_price))
+
+print(price_with_tax)
+print(type(price_with_tax))
+
+print(discount_available)
+print(type(discount_available))
+
+print(wave_component)
+print(type(wave_component))
