c07282be4a069b7c606f92a89e39f2648b78c6b0
XploreCodeBase <support@xplorehub.pl>
2025-06-29 11:08:51 +0200

    Raport (rozwiązanie)
    Plik z rozwiązaniem projektu: Raport
    Wewnątrz znajdziesz także praktyczne notatki, otwórz go w swoim edytorze kodu i przetestuj go - to najefektywniejszy sposób na naukę.

---
+++ raport.py
@@ -0,0 +1,31 @@
+"""
+Mini-projekt: Raport tekstowy z użyciem znaków specjalnych
+Cel: pokazanie praktycznego wykorzystania \n i \t w formatowaniu tekstu
+"""
+
+# Zmienna z nazwą projektu
+project_name = "Analiza sprzedaży"
+
+# Zmienna z autorem raportu
+author = "Alicja"
+
+# Zmienna z wersją raportu
+version = 1.0
+
+# Wyświetlamy raport
+# \n tworzy nową linię
+# \t dodaje tabulator (wyrównanie)
+print("Raport projektu:", project_name)
+print("Autor:\t\t", author)
+print("Wersja:\t\t", version)
+
+# Dodajemy puste linie i końcowe podziękowanie
+# Wszystko w jednym wywołaniu print()
+print("\nDziękujemy za skorzystanie z raportu.\nZapraszamy ponownie.\nMiłego dnia!")
+
+"""
+Przypomnienie:
+- \n oznacza nową linię (newline).
+- \t oznacza tabulator (tab).
+- Jeśli widzisz dziwny układ tekstu, sprawdź, czy na pewno używasz prawidłowych znaków ucieczki.
+"""
